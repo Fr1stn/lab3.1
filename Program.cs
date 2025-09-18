ac8460fa5d1fe3792f085161fe8a9c2b774e5650
Fr1stn <happipampers00@gmail.com>
2025-09-19 00:35:51 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите начальное число (m):");
+double m = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите конечное число (n):");
+double n = Convert.ToDouble(Console.ReadLine());
+double S = 0;
+double i = m;
+while (i <= n)
+{
+    i += 0.5;
+    S += i;
+}
+Console.WriteLine(S);
